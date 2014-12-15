b844c0d32b94f61265c2f050a40e6aab5e2e150c
hammadakhtar <hammadakhtar078@gmail.com>
2014-12-15 08:30:05 +0000

    Add user log out

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -19,7 +19,7 @@ class UserSignupTest < ActionDispatch::IntegrationTest
                                             password:              "password",
                                             password_confirmation: "password"}
     end
-    
     assert_template "users/show"
+    assert is_logged_in?
   end
 end
