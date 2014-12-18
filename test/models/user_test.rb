4ec3607540dee03bac0d359a0648c141250fd9ff
hammadakhtar <hammadakhtar078@gmail.com>
2014-12-18 12:19:14 +0000

    Fixed two log-out bugs

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -68,11 +68,11 @@ class UserTest < ActiveSupport::TestCase
   
   test "password length should have a minimum length" do
     @user.password = @user.password_confirmation = "a"*5
-    
     assert_not @user.valid?
-      
   end
   
-  
+  test "authenticated? should return false when digest is nil" do
+    assert_not @user.authenticated?('')
+  end
   
 end
